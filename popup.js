524d63ebaa99a78dae06d83f85817259ec45b3cd
Austin Smith <austin.smith@playags.com>
2019-01-09 14:11:41 -0600

    add content script, options, and popup

--- popup.js
+++ popup.js
@@ -1,11 +1,5 @@
-//
-// 1.get input from omnibar
-//
-// 2.split input string into array
-//
-// 3. use optparse (or write own) to parse array and execute command functions
-//
-//
+/**
+ * popup.js - runs on popup.html
+ */
 
-
-window.open('http://google.com');
+// code
